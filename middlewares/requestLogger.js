ea2fb8e90b1f14f61fad99865032de882a98c032
Vladimir Mikhailov <vladimir@mikhailov.in>
2021-07-28 22:21:07 +0300

    feat: request logger

---
+++ middlewares/requestLogger.js
@@ -0,0 +1,20 @@
+const winston = require('winston');
+const expressWinston = require('express-winston');
+const DailyRotateFile = require('winston-daily-rotate-file');
+
+const requestLogger = expressWinston.logger({
+  transports: [
+    // new winston.transports.File({ filename: './logs/request.log' }),
+    new DailyRotateFile({
+      filename: './logs/request-%DATE%.log',
+      datePattern: 'YYYY-MM-DD',
+      zippedArchive: true,
+      maxSize: '10m',
+      maxFiles: '7d',
+    }),
+  ],
+  format: winston.format.json(),
+  colorize: true,
+});
+
+module.exports = requestLogger;
