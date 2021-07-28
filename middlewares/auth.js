9483564f381e0e635934e9976dccb9d68dbbc168
Vladimir Mikhailov <vladimir@mikhailov.in>
2021-07-28 22:15:19 +0300

    feat: auth middleware

---
+++ middlewares/auth.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken');
+const UnauthorizedError = require('../errors/401');
+
+const { JWT_SECRET = 'dev-secret' } = process.env;
+
+const handleAuthError = () => {
+  throw new UnauthorizedError('Ошибка авторизации');
+};
+
+module.exports = (req, res, next) => {
+  try {
+    if (!req.cookies.jwt) handleAuthError();
+
+    const token = req.cookies.jwt;
+
+    req.user = jwt.verify(token, JWT_SECRET);
+  } catch (err) {
+    handleAuthError();
+  }
+  next();
+};
