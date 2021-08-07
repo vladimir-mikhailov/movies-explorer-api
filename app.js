78144714d335fb1f8cccb139bed798cb054eaaef
Vladimir Mikhailov <vladimir@mikhailov.in>
2021-08-07 18:45:16 +0300

    refactor: cors off

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(cors);
+// app.use(cors); todo включить cors перед деплоем
 
 app.use(router);
 
